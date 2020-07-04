367abdab4e3d3bb6dbf193a7b77d0a5b7c1e33f4
srnazng <srnazng@gmail.com>
2020-07-04 17:37:46 -0400

    Updates

---
+++ precache-manifest.4ba14af1574e0b57339ad67ad502d68b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6997fade0e60303dbb5287a4647b91f2",
+    "url": "/ticketSystem/index.html"
+  },
+  {
+    "revision": "271faab06814e11fcb11",
+    "url": "/ticketSystem/static/css/main.6f708721.chunk.css"
+  },
+  {
+    "revision": "f2c428c93ebab6d6a2f5",
+    "url": "/ticketSystem/static/js/2.d70bab07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ticketSystem/static/js/2.d70bab07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "271faab06814e11fcb11",
+    "url": "/ticketSystem/static/js/main.cf9a1098.chunk.js"
+  },
+  {
+    "revision": "815be489eaed9541c9db",
+    "url": "/ticketSystem/static/js/runtime-main.1f2b464a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c00327a180e360152f60cb573522875b.js"
+  "/ticketSystem/precache-manifest.4ba14af1574e0b57339ad67ad502d68b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ticketSystem/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
