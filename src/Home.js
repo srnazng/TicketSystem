f82aeecade87f67b582b9436eef5244e19303bd5
srnazng <srnazng@gmail.com>
2020-07-04 16:53:06 -0400

    reopen feature

--- src/Home.js
+++ src/Home.js
@@ -9,39 +9,54 @@ class Home extends Component {
 
         this.state = {
             items: [],
+            claimedItems: [],
+            openItems: [],
             name: props.name
         }
 
         this.addItem = this.addItem.bind(this);
         this.deleteItem = this.deleteItem.bind(this);
         this.getName = this.getName.bind(this);
+        this.claimItem = this.claimItem.bind(this);
+        this.reopenItem = this.reopenItem.bind(this);
     }
 
     addItem(e) {
         if (this._inputElement1.value !== "") {
             var dateNow = new Date();
             var hours = 0;
+            var minutes = 0;
             var end = "AM";
             if (dateNow.getHours() > 12) {
                 hours = (dateNow.getHours() % 12);
                 end = "PM"
             }
-            var timeNow = hours + ':' + dateNow.getMinutes() + end;
-            console.log("time submitted: ", timeNow);
+            if (dateNow.getMinutes() < 10) {
+                minutes = "0" + dateNow.getMinutes();
+            }
+            else {
+                minutes = dateNow.getMinutes();
+            }
+
+            var timeNow = hours + ':' + minutes + " " + end;
+            //console.log("time submitted: ", timeNow);
+
             var newItem = {
                 name: this.state.name,
                 problem: this._inputElement1.value,
                 location: this._inputElement2.value,
                 contact: this._inputElement3.value,
                 timeSubmitted: timeNow,
+                isClaimed: 0,
                 key: Date.now()
             };
 
-            console.log("time submitted: ", this.timeSubmitted);
+            console.log("time submitted: ", newItem.timeSubmitted);
 
             this.setState((prevState) => {
                 return {
-                    items: prevState.items.concat(newItem)
+                    items: prevState.items.concat(newItem),
+                    openItems: prevState.openItems.concat(newItem)
                 };
             });
         }
@@ -59,12 +74,83 @@ class Home extends Component {
     }
 
     deleteItem(key) {
+        // remove from list of all items
         var filteredItems = this.state.items.filter(function (item) {
             return (item.key !== key)
         });
 
         this.setState({
             items: filteredItems
+        });
+
+        // remove from list of open items
+        filteredItems = this.state.openItems.filter(function (item) {
+            return (item.key !== key)
+        });
+
+        this.setState({
+            openItems: filteredItems
+        });
+
+        // remove from list of claimed items
+        filteredItems = this.state.claimedItems.filter(function (item) {
+            return (item.key !== key)
+        });
+
+        this.setState({
+            claimedItems: filteredItems
+        });
+    }
+
+
+    claimItem(key) {
+        // Add item to list of claimed items
+        var newlyClaimedItem = this.state.items.filter(function (item) {
+            return (item.key == key)
+        });
+        console.log("newlyClaimedItem: ", newlyClaimedItem);
+
+        this.setState((prevState) => {
+            return {
+                claimedItems: prevState.claimedItems.concat(newlyClaimedItem)
+            };
+        });
+
+        // Delete item from list of opened items
+        var filteredItems = this.state.openItems.filter(
+            function (item) {
+                return (item.key !== key)
+            }
+        );
+
+        this.setState({
+            openItems: filteredItems
+        });
+
+    }
+
+    reopenItem(key) {
+        console.log("reopen");
+        // find from list of claimItems and add to list of open items
+        var newlyOpenedItem = this.state.claimedItems.filter(function (item) {
+            return (item.key == key)
+        });
+
+        this.setState((prevState) => {
+            return {
+                openItems: prevState.openItems.concat(newlyOpenedItem)
+            };
+        });
+
+        // Delete item from list of claimed items
+        var filteredItems = this.state.claimedItems.filter(
+            function (item) {
+                return (item.key !== key)
+            }
+        );
+
+        this.setState({
+            claimedItems: filteredItems
         });
     }
 
@@ -73,7 +159,7 @@ class Home extends Component {
     }
 
     render() {
-        console.log(this.props.name)
+        // console.log(this.props.name)
         return (
             <div className="todoListMain">
 
@@ -100,8 +186,13 @@ class Home extends Component {
                     </form>
                 </div>
 
-                <TodoItems entries={this.state.items}
+                <TodoItems
+                    entries={this.state.items}
+                    claimedEntries={this.state.claimedItems}
+                    openEntries={this.state.openItems}
                     delete={this.deleteItem}
+                    claimItem={this.claimItem}
+                    reopenItem={this.reopenItem}
                 />
 
             </div>
--- src/TodoItems.js
+++ src/TodoItems.js
@@ -6,7 +6,10 @@ class TodoItems extends Component {
         super(props);
 
         this.createTasks = this.createTasks.bind(this);
+        this.createClaimedTasks = this.createClaimedTasks.bind(this);
         this.delete = this.delete.bind(this);
+        this.claimItem = this.claimItem.bind(this);
+        this.reopenItem = this.reopenItem.bind(this);
     }
 
     createTasks(item) {
@@ -28,8 +31,34 @@ class TodoItems extends Component {
                         <br />
                     <b>Contact: </b>{item.contact}
                 </p>
-                <form onSubmit={e => { e.preventDefault(); this.delete(item.key) }}>
+                <form onSubmit={e => { e.preventDefault(); this.claimItem(item.key) }}>
                     <button type="submit">CLAIM TICKET</button>
+                </form>
+            </li >
+        </div>
+    }
+
+    createClaimedTasks(item) {
+        return <div>
+            <li key={item.key}>
+                <h3>
+                    {item.name}
+                    <button onClick={e => { e.preventDefault(); this.delete(item.key) }} className="xButton">
+                        x
+                    </button>
+                    <br />
+                    <div className="time">{item.timeSubmitted}</div>
+                </h3>
+                <p>
+                    <br />
+                    {item.problem}
+                    <br />
+                        ({item.location})
+                        <br />
+                    <b>Contact: </b>{item.contact}
+                </p>
+                <form onSubmit={e => { e.preventDefault(); this.reopenItem(item.key) }}>
+                    <button type="submit">REOPEN TICKET</button>
                 </form>
             </li >
         </div>
@@ -39,14 +68,32 @@ class TodoItems extends Component {
         this.props.delete(key);
     }
 
+    claimItem(key) {
+        this.props.claimItem(key);
+    }
+
+    reopenItem(key) {
+        this.props.reopenItem(key);
+    }
+
+
     render() {
+        console.log(this.props);
         var todoEntries = this.props.entries;
-        var listItems = todoEntries.map(this.createTasks)
+        var claimedEntries = this.props.claimedEntries;
+        var openEntries = this.props.openEntries;
+        var openListItems = openEntries.map(this.createTasks);
+        var claimedListItems = claimedEntries.map(this.createClaimedTasks);
 
         return (
             <ul className="theList">
                 <FlipMove duration={150} easing="ease-out">
-                    {listItems}
+                    <div className="openTickets">
+                        {openListItems}
+                    </div>
+                    <div className="claimedTickets">
+                        {claimedListItems}
+                    </div>
                 </FlipMove>
             </ul>
         )
